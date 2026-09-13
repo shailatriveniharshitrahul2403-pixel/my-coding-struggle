dd58896248476030564d8f3478858d02ed2c1edb
pand4ycodes <rahulpand4y@gmail.com>
2026-09-13 19:07:15 +0530

    added my actual code files

---
+++ bool.java
@@ -0,0 +1,10 @@
+
+
+public class bool{
+    public static void main(String[] args) {
+        boolean newTarget=true;
+        boolean oldTarget=false;
+        IO.println("i love java:"+newTarget);
+        IO.println("i hate java:"+oldTarget);
+    }
+}
