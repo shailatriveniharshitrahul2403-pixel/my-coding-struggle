f5b641b2ca66162cb532663ac4b03549cc733924
pand4ycodes <rahulpand4y@gmail.com>
2026-09-13 18:56:31 +0530

    added my actual code files

---
+++ variable.java
@@ -0,0 +1,18 @@
+class variable{
+    public static void main(String[] args) {
+        int MyInt =78;
+        String myname ="HARSHIT pANDEY";
+        boolean mybOOl=true;
+        char s='l';
+        double myDouble=89.90;
+IO.println(mybOOl);
+IO.println(s);
+IO.println(MyInt);
+IO.println(myDouble);
+IO.println(myname);
+IO.println(mybOOl);
+
+        
+    }
+
+}
