f903e52420d7e404f91435dcecf7d4d2a2609114
pand4ycodes <rahulpand4y@gmail.com>
2026-09-13 19:15:48 +0530

    added my actual code files

---
+++ bite.java
@@ -0,0 +1,9 @@
+public  class bite {
+    public static void main(String[]args){
+        byte age=90;
+        byte tem=-90;
+        IO.println(age^2);
+        IO.println(tem-tem);
+        
+    }
+}
